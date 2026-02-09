1731495a90db4c43d034c69ce710ed623617bc00
celestenicole <celestelluendelgado05@gmail.com>
2026-02-09 16:31:03 -0500

    db

--- db.js
+++ db.js
@@ -1,13 +1,19 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  allowExitOnIdle: true
-})
+const pool = process.env.DATABASE_URL
+  ? new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false },
+      allowExitOnIdle: true
+    })
+  : new Pool({
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      allowExitOnIdle: true
+    })
 
 module.exports = pool
